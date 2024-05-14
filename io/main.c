82d0ef0250b89ab93f85fa9b3a54c4c5d3302082
sanghuck_yu <sanghuck_yu@tmax.co.kr>
2024-05-14 10:21:27 +0900

    seperate filename

--- io/main.c
+++ io/main.c
@@ -15,16 +15,18 @@ void measure_time(void (*function)(const char*, size_t), const char* filename, s
 }
 
 int main() {
-    const char* filename = "data.bin";
+    const char* filename_kb = "data_kb.bin";
+    const char* filename_mb = "data_mb.bin";
+    const char* filename_gb = "data_gb.bin";
 
     // 1KB를 block_write
-    measure_time(block_write_kb, filename, 1, "1KB");
+    measure_time(block_write_kb, filename_kb, 1, "1KB");
 
     // 1MB를 block_write
-    measure_time(block_write_mb, filename, 1, "1MB");
+    measure_time(block_write_mb, filename_mb, 1, "1MB");
 
     // 1GB를 block_write
-    measure_time(block_write_gb, filename, 1, "1GB");
+    measure_time(block_write_gb, filename_gb, 1, "1GB");
 
     return 0;
 }
