b6daa1af0391d1d336b7e1fc4764a043d3dec49f
sanghuck_yu <sanghuck_yu@tmax.co.kr>
2024-05-14 15:41:40 +0900

    fix truncated read

--- io/block_io.c
+++ io/block_io.c
@@ -102,22 +102,97 @@ void block_io_read(const char* filename, sample* samples, int cnt) {
     int offset = 0;
 
     for (int i = 0; i < cnt; ++i) {
+        // 필드를 읽을 때마다 블록을 확인하고 필드가 블록을 넘어가는 경우 처리합니다.
         if (offset == 0) {
             read(fd, blk, BLOCK_SIZE);
         }
 
-        memcpy(&samples[i].c, &blk[offset], sizeof(samples[i].c));
-        offset += sizeof(samples[i].c);
+        // 각 필드가 블록을 넘어가는지 확인하고 처리합니다.
+        // 필드가 블록을 넘어가는 경우에는 블록을 나누어 읽어와야 합니다.
+        if (offset + sizeof(samples[i].c) > BLOCK_SIZE) {
+            int pre_blk = BLOCK_SIZE - offset;
+            int post_blk = sizeof(samples[i].c) - pre_blk;
 
-        memcpy(&samples[i].i, &blk[offset], sizeof(samples[i].i));
-        offset += sizeof(samples[i].i);
+            // c의 일부를 읽어옵니다.
+            memcpy(&samples[i].c, &blk[offset], pre_blk);
+            offset = 0;
 
-        memcpy(&samples[i].l, &blk[offset], sizeof(samples[i].l));
-        offset += sizeof(samples[i].l);
+            // 다음 블록을 읽어옵니다.
+            read(fd, blk, BLOCK_SIZE);
 
-        memcpy(&samples[i].d, &blk[offset], sizeof(samples[i].d));
-        offset += sizeof(samples[i].d);
+            // c의 나머지를 읽어옵니다.
+            memcpy(((char*)&samples[i].c) + pre_blk, &blk[offset], post_blk);
+            offset += post_blk;
+        } else {
+            // 블록을 넘어가지 않는 경우 필드를 그대로 읽어옵니다.
+            memcpy(&samples[i].c, &blk[offset], sizeof(samples[i].c));
+            offset += sizeof(samples[i].c);
+        }
 
+        // i 필드를 처리합니다.
+        if (offset + sizeof(samples[i].i) > BLOCK_SIZE) {
+            int pre_blk = BLOCK_SIZE - offset;
+            int post_blk = sizeof(samples[i].i) - pre_blk;
+
+            // i의 일부를 읽어옵니다.
+            memcpy(&samples[i].i, &blk[offset], pre_blk);
+            offset = 0;
+
+            // 다음 블록을 읽어옵니다.
+            read(fd, blk, BLOCK_SIZE);
+
+            // i의 나머지를 읽어옵니다.
+            memcpy(((char*)&samples[i].i) + pre_blk, &blk[offset], post_blk);
+            offset += post_blk;
+        } else {
+            // 블록을 넘어가지 않는 경우 필드를 그대로 읽어옵니다.
+            memcpy(&samples[i].i, &blk[offset], sizeof(samples[i].i));
+            offset += sizeof(samples[i].i);
+        }
+
+        // l 필드를 처리합니다.
+        if (offset + sizeof(samples[i].l) > BLOCK_SIZE) {
+            int pre_blk = BLOCK_SIZE - offset;
+            int post_blk = sizeof(samples[i].l) - pre_blk;
+
+            // l의 일부를 읽어옵니다.
+            memcpy(&samples[i].l, &blk[offset], pre_blk);
+            offset = 0;
+
+            // 다음 블록을 읽어옵니다.
+            read(fd, blk, BLOCK_SIZE);
+
+            // l의 나머지를 읽어옵니다.
+            memcpy(((char*)&samples[i].l) + pre_blk, &blk[offset], post_blk);
+            offset += post_blk;
+        } else {
+            // 블록을 넘어가지 않는 경우 필드를 그대로 읽어옵니다.
+            memcpy(&samples[i].l, &blk[offset], sizeof(samples[i].l));
+            offset += sizeof(samples[i].l);
+        }
+
+        // d 필드를 처리합니다.
+        if (offset + sizeof(samples[i].d) > BLOCK_SIZE) {
+            int pre_blk = BLOCK_SIZE - offset;
+            int post_blk = sizeof(samples[i].d) - pre_blk;
+
+            // d의 일부를 읽어옵니다.
+            memcpy(&samples[i].d, &blk[offset], pre_blk);
+            offset = 0;
+
+            // 다음 블록을 읽어옵니다.
+            read(fd, blk, BLOCK_SIZE);
+
+            // d의 나머지를 읽어옵니다.
+            memcpy(((char*)&samples[i].d) + pre_blk, &blk[offset], post_blk);
+            offset += post_blk;
+        } else {
+            // 블록을 넘어가지 않는 경우 필드를 그대로 읽어옵니다.
+            memcpy(&samples[i].d, &blk[offset], sizeof(samples[i].d));
+            offset += sizeof(samples[i].d);
+        }
+
+        // 다음 필드를 읽을 때 블록을 넘어갈 경우 offset을 초기화합니다.
         if (offset >= BLOCK_SIZE) {
             offset = 0;
         }
