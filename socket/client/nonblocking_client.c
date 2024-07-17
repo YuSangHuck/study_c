c664e24a4eb0bff8474a89b69090ee97a062bdeb
sanghuck_yu <sanghuck_yu@tmax.co.kr>
2024-07-17 10:02:19 +0900

    ip 대신 hostname으로 dependency 제거

--- socket/client/nonblocking_client.c
+++ socket/client/nonblocking_client.c
@@ -1,14 +1,15 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define PORT 8080
-#define SERVER_IP                                                              \
-    "172.18.0.3" // Use the service name defined in docker-compose.yml
+#define SERVER_NAME                                                            \
+    "nonblocking_server" // Docker Compose에서 정의한 서비스 이름
 
 int
 main() {
@@ -22,17 +23,19 @@ main() {
         return -1;
     }
 
-    // Set the socket to non-blocking mode
     int flags = fcntl(sock, F_GETFL, 0);
     fcntl(sock, F_SETFL, flags | O_NONBLOCK);
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
+    struct hostent *server = gethostbyname(SERVER_NAME);
+    if (server == NULL) {
+        fprintf(stderr, "Error: Unable to resolve hostname\n");
         return -1;
     }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr_list[0],
+           server->h_length);
 
     while (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) <
            0) {
